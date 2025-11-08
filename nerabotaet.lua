b44b55e5a04f1256faa41c92085a294632912d23
DEBIL59195 <prodavec4466@gmail.com>
2025-11-09 00:28:00 +0500

    Update nerabotaet.lua

--- nerabotaet.lua
+++ nerabotaet.lua
@@ -490,7 +490,7 @@ local ICONS = {
 }
 local ESP_SETTINGS = { MaxDistance = 500, Font = Enum.Font.GothamBold, Color = Color3.fromRGB(148, 0, 211),
     BgColor = Color3.fromRGB(24, 16, 40), TxtColor = Color3.fromRGB(225, 210, 255), TextSize = 16 }
-local OBJECT_EMOJIS = {['La Vacca Saturno Saturita'] = '🐮', ['Nooo My Hotspot'] = '👽', ['La Supreme Combinasion'] = '🔫',['La Taco Combinasion'] = '👒',['Mariachi Corazoni'] = '💀',['Tacorita Bicicleta'] = '🚵‍♂️',['1x1x1x1'] = '🈯️',
+local OBJECT_EMOJIS = {['La Vacca Saturno Saturita'] = '🐮', ['Nooo My Hotspot'] = '👽', ['La Supreme Combinasion'] = '🔫',['La Taco Combinasion'] = '👒',['Mariachi Corazoni'] = '💀',['Tacorita Bicicleta'] = '🚵‍♂️',['1x1x1x1'] = '🈯️',['Cooki and Milki'] = '🍪',['Los Puggies'] = '🦮',
     ['Ketupat Kepat'] = '⚰️',['Graipuss Medussi'] = '🦑',['Torrtuginni Dragonfrutini'] = '🐢',['Tictac Sahur'] = '🕰',["Tang Tang Keletang"] = "📢",["Money Money Puggy"] = "🐶",["Los Primos"] = "🙆‍♂️",['Los Tacoritas'] = '🚴',['Guest 666'] = '㊙️',
     ['Pot Hotspot'] = ' 📱',['La Grande Combinasion'] = '❗️',['Garama and Madundung'] = '🥫',['La Spooky Grande'] = '🟧',['Spooky and Pumpky'] = '🎃',['La Casa Boo'] = '👁‍🗨',["Burrito Bandito"] = "👮‍♀️",["Capitano Moby"] = "🚢",
     ['Secret Lucky Block'] = '⬛️',['Strawberry Elephant'] = '🐘',['Nuclearo Dinossauro'] = '🦕',['Spaghetti Tualetti'] = '🚽',['Meowl'] = '🐈',['Mieteteira Bicicleteira'] = '☠️',['Headless Horseman'] = '🐴',
